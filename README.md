bb79c65867c57f6aadb4df282cd276e279efffaa
unknown <bongthoi1111@gmail.com>
2018-11-01 11:59:48 +0700

    structure project

--- README.md
+++ README.md
@@ -3,8 +3,28 @@
 ## Features:
 - [x] Setup Enviroment.
 - [x] ES2015,ES2017.
-- [x] Nodemon
 
+## Structure's Project:
+--\root
+      |
+      \configs
+             |
+             \server_config.json   
+      \public
+      \src
+         |
+         \models
+         \repositories
+         \services
+         \controllers
+                    |
+                    \index.js
+         \utilities
+      \views
+           |
+           \index.ejs
+      \index.js
+        
 ## Installation
 1. Clone the Repo.
 2. Go into the folder and hit the npm install command
--- index.js
+++ index.js
@@ -1,16 +1,65 @@
 import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
+import flash from 'connect-flash';
+import session from 'express-session';
+import fileupload from 'express-fileupload';
 
+/** */
+import SERVER_CONFIG from './configs/server_config.json';
+import controller from './src/controllers/index';
 
 /** */
 var app=express();
-var PORT=3000;
+
+/**config MiddleAware */
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(expressValidator());
+app.use(flash());
+app.use(fileupload());
+
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: true,
+}));
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
+app.use(function(req, res, next){
+	res.locals.success_message = req.flash('success_message');
+	res.locals.error_message = req.flash('error_message');
+	res.locals.error = req.flash('error');
+	res.locals.errors = req.flash('errors');
+	res.locals.user = req.user || null;
+  	next();
+});
+
+app.use('/', controller);
 
 
-
-
-
-var webserver=app.listen(PORT,()=>{
-    console.log("Listening on port: "+webserver.address().port);
+/** */
+var server=app.listen(SERVER_CONFIG.server_port,()=>{
+    console.log(`Server running at http://${SERVER_CONFIG.server_ip}:${server.address().port}/`);
 });
